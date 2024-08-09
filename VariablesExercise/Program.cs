d94744337362da376b509e56bd59bbbd54dbdef9
Raynard Brown <brown_raynard@icloud.com>
2024-08-09 09:29:26 -0500

    variable

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace VariablesExercise
+﻿using System.Reflection.PortableExecutable;
+
+namespace VariablesExercise
 {
     public class Program
     {
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            string dogName = "Max"; 
+            int number = 4;
+            char pup = '1';
+            int Pup = '1';
+            bool isBiggestDog = true;
+            double height = 6.2;
+            decimal money = 350.00m;
+
+        Console.WriteLine($"I have and English Mastiff named {dogName} he is about {number} years old.");
+        Console.WriteLine($"{dogName} also had puppies I did keep {pup}.");
+        Console.WriteLine($"It's {isBiggestDog} he is biggest dog i've had and tallest on his back leg he stood about {height } and also cost about {money} each vet visit.");
+
     }
+
 }
+
+}
+
